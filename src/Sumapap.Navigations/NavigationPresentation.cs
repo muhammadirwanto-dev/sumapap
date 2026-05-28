c0ac7a92c2f39b8bb2a6c9d18b38629217722262
muhammadirwanto-dev <muhammadirwanto.dev@gmail.com>
2026-05-28 17:22:55 +0700

    feat(additional): add navigations module

---
+++ src/Sumapap.Navigations/NavigationPresentation.cs
@@ -0,0 +1,11 @@
+﻿namespace Sumapap.Navigations
+{
+    /// <summary>
+    /// Describes how the destination should be presented.
+    /// </summary>
+    public enum NavigationPresentation
+    {
+        Normal,
+        Modal
+    }
+}
